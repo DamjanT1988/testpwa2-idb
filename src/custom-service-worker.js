9a3b0e95655fd1c147261cc01685765509f54635
DamjanT1988 <Damjan_t@hotmail.com>
2023-10-31 10:03:57 +0100

    11

--- src/custom-service-worker.js
+++ src/custom-service-worker.js
@@ -14,21 +14,19 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-            if (cachedResponse) {
-                return cachedResponse;
-            }
-
-            return fetch(event.request).then((response) => {
-                return caches.open('image-cache').then((cache) => {
-                    // Make sure to clone the response object to not interfere with the browser's main fetch functionality
-                    cache.put(event.request, response.clone());
-                    return response;
+        fetch(event.request).catch(() => {
+            return caches.match(event.request)
+                .then((response) => {
+                    if (response) {
+                        return response; // if match is found in the cache, return it
+                    } else if (event.request.headers.get('accept').includes('text/html')) {
+                        return caches.match('/offline.html'); // if not found, and the request is for a HTML page, return offline.html
+                    }
                 });
-            });
         })
     );
 });
+
 
 
 //Store with fetch API
