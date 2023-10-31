00cacd77c3ff10675d9e586c8c42e4c8e74865ff
DamjanT1988 <Damjan_t@hotmail.com>
2023-10-31 12:01:09 +0100

    29

---
+++ public/custom-service-worker.js
@@ -0,0 +1,30 @@
+// service-worker.js
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('app-cache').then(function(cache) {
+            return cache.addAll([
+                './',
+                'page1.html',
+                'page2.html'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request).then(function(response) {
+                let responseClone = response.clone();
+                caches.open('app-cache').then(function(cache) {
+                    cache.put(event.request, responseClone);
+                });
+
+                return response;
+            });
+        }).catch(function() {
+            return caches.match('/offline.html'); // Optionally, return a custom offline page
+        })
+    );
+});
+
